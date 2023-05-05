de9fda8c32b72c56dcbb9390b3c05482971a6ab6
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-05 16:17:43 +0200

    improved function with parent construct

--- Models/Food.php
+++ Models/Food.php
@@ -7,6 +7,12 @@ class Food extends Products{
     public $species;
 
 
+    public function __construct($_price,$_brand, Species $_species)
+    {
 
+        parent::__construct($_price,$_brand);
+        $this->species = $_species;
+
+    }
 
 }
--- index.php
+++ index.php
@@ -10,15 +10,17 @@ require_once __DIR__ . '/Models/Species.php';
 
 $cibo = new Products(100,"peppe");
 
-$gatto = new Species ("cat");
-$croccantini = new Food(2,"pippo");
+// $gatto = new Species ("cat");
+$species = new Species ("cat");
+$croccantini = new Food(2,"pippo",$species);
 
-$species = new Species("cat");
+$species = new Species("dog");
+$umido = new Food(5,"pluto",$species);
 
-
-$croccantini->species = $species;
+// $croccantini->species = $species;
 
 var_dump($croccantini);
+var_dump($umido);
 
 
 
